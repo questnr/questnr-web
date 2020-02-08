6459cb7a3c332ab8b1e9f3244e865157c4b60b59
Satish Kumar Gaur <2395424@AFRICA.AIRTEL.ITM>
2020-02-08 11:19:37 +0530

    theme change

--- src/app/feeds-frame/feeds/trending-feeds/trending-feeds.component.ts
+++ src/app/feeds-frame/feeds/trending-feeds/trending-feeds.component.ts
@@ -6,9 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trending-feeds.component.scss']
 })
 export class TrendingFeedsComponent implements OnInit {
+  public postId = 2;
+  public userIcon = 'https://material.angular.io/assets/img/examples/shiba2.jpg';
+  public postMedia: any = '';
+  public  userName = 'Andrew Garfield';
+  public profileHighlight: any = '10,000 followers';
+  public postContent: string = 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.\n' +
+    '        A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally\n' +
+    '        bred for hunting.';
+  public postedOn: any = '10 hr';
+  public  likes = 100;
+  public comments  = 2500;
+  public toggleComment = false;
 
   constructor() { }
 
+  toggleCommentSection(postid) {
+   const elementById = document.getElementById(postid);
+   if (elementById.style.display == 'none') {
+     elementById.style.display = 'block';
+   } else {
+     elementById.style.display = 'none';
+   }
+  }
   ngOnInit() {
   }
 
