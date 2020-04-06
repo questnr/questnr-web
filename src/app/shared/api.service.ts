841a06c58f126563504c06dab8e9d2619c8816be
Amit Kumar Thakur <amit@ritaumsolutions.com>
2020-04-06 10:26:25 +0530

    landing page redesign

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getTopHashtags() {
+    return this.http.get(this.baseUrl + 'hash-tag-with-highest-rank');
+  }
+  getTopUsers() {
+    return this.http.get(this.baseUrl + 'users-with-highest-rank');
+  }
+}
