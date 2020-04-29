5f03274c7699840db86a1c7f58ea883b4e88bd0a
brijeshlakkad <brijeshlakkad22@gmail.com>
2020-04-29 13:39:16 +0530

    baseURL changed for prod to ip of backend instance

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
-  // baseUrl: 'https://www.sats.questnr.com/api/v1/',
-  baseUrl: 'https://5bf442c9.ngrok.io//api/v1/',
+  baseUrl: 'https://internal.questnr.com/api/v1/',
   // baseUrl: 'http://localhost:3010/api/v1/',
   googleKey: '836632017511-na1k4gagi79qlvdp644q1shd0rjffoc9.apps.googleusercontent.com',
   fbKey: '508905489804238',
