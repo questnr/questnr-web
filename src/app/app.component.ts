06589fdc50211f748aeb1603805e7c440352933c
brijeshlakkad <brijeshlakkad22@gmail.com>
2020-05-13 12:27:38 +0530

    receiving the FCM messages with data. Data Notification completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 // @ts-ignore
-import { AngularFireMessaging } from '@angular/fire/messaging';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -10,31 +9,16 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(public translate: TranslateService, private angularFireMessaging: AngularFireMessaging) {
+  constructor(public translate: TranslateService) {
     translate.addLangs(['en', 'hn']);
     translate.setDefaultLang('hr');
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|hn/) ? browserLang : 'hr');
   }
   ngOnInit() {
-    // Receive notification messages
-    this.receiveMessage();
-  }
 
+  }
   test() {
     alert('parent');
-  }
-
-  /**
-  * hook method when new notification received in foreground
-  */
-  receiveMessage() {
-    this.angularFireMessaging.onMessage((message) => {
-      console.log("received a message:", message);
-      if (typeof message !== 'undefined' && typeof message.fcmOptions !== 'undefined' && typeof message.fcmOptions.link !== 'undefined') {
-        // window.open(message.fcmOptions.link, "_blank");
-        // @Todo: increament notification count and highlight it.
-      }
-    });
   }
 }
