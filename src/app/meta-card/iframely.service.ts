70ddad99914009003ea939aa4d093c2a2a9447da
brijeshlakkad <brijeshlakkad22@gmail.com>
2020-05-14 13:00:54 +0530

    Detecting the link when user types the link and shows the meta-card of the link

---
+++ src/app/meta-card/iframely.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { IFramelyData } from 'models/iframely.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IFramelyService {
+
+  apiKey: string = "b45f06cd090f9e75148e93";
+  iframelyLink: string = "http://iframe.ly/api/iframely";
+
+  constructor(private http: HttpClient) { }
+
+
+  iFramelyData: IFramelyData = new IFramelyData();
+
+  getIFramelyData(detectedLink: string) {
+    return this.http.get(this.iframelyLink, { params: { url: detectedLink, api_key: this.apiKey } }).toPromise().then((iFramelyResp: any) => {
+      return new Promise((resolve, reject) => {
+        if (iFramelyResp.error) {
+          this.iFramelyData.error = true;
+        }
+        else {
+          this.iFramelyData.error = false;
+          this.iFramelyData.url = iFramelyResp.url;
+          this.iFramelyData.title = iFramelyResp.meta.title;
+          this.iFramelyData.description = iFramelyResp.meta.description;
+          if (iFramelyResp.links.thumbnail.length > 0) {
+            this.iFramelyData.thumbnailLink = iFramelyResp.links.thumbnail[0].href;
+          }
+          if (iFramelyResp.links.icon.length > 0) {
+            this.iFramelyData.iconLink = iFramelyResp.links.icon[0].href;
+          }
+        }
+        resolve(this.iFramelyData);
+      });
+    });
+  }
+}
