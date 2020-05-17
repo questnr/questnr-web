82ac0b6759d880e3fda02cbf5ce6df3478a8d1b0
Satish Kumar Gaur <satishkumargaur@Satishs-MacBook-Air.local>
2020-05-17 12:48:00 +0530

    iframelyService

--- src/app/meta-card/iframely.service.ts
+++ src/app/meta-card/iframely.service.ts
@@ -8,35 +8,31 @@ import { IFramelyData } from 'models/iframely.model';
 })
 export class IFramelyService {
 
-  apiKey = 'b45f06cd090f9e75148e93';
-  iframelyLink = 'http://iframe.ly/api/iframely';
+  apiKey: string = "b45f06cd090f9e75148e93";
+  iframelyLink: string = "https://iframe.ly/api/iframely";
 
   constructor(private http: HttpClient) { }
 
 
   iFramelyData: IFramelyData = new IFramelyData();
 
-
-  getIFramelyData(detectedLink: string) {
-    // tslint:disable-next-line:max-line-length
-    return this.http.get(this.iframelyLink, { params: { url: detectedLink, api_key: this.apiKey } }).toPromise().then((iFramelyResp: any) => {
-      return new Promise((resolve, reject) => {
-        if (iFramelyResp.error) {
-          this.iFramelyData.error = true;
-        } else {
-          this.iFramelyData.error = false;
-          this.iFramelyData.url = iFramelyResp.url;
-          this.iFramelyData.title = iFramelyResp.meta.title;
-          this.iFramelyData.description = iFramelyResp.meta.description;
-          if (iFramelyResp.links.thumbnail.length > 0) {
-            this.iFramelyData.thumbnailLink = iFramelyResp.links.thumbnail[0].href;
-          }
-          if (iFramelyResp.links.icon.length > 0) {
-            this.iFramelyData.iconLink = iFramelyResp.links.icon[0].href;
-          }
-        }
-        resolve(this.iFramelyData);
-      });
-    });
+  async getIFramelyData(detectedLink: string): Promise<IFramelyData> {
+    let iFramelyResp: any = await this.http.get(this.iframelyLink, { params: { url: detectedLink, api_key: this.apiKey } }).toPromise();
+    if (!iFramelyResp || iFramelyResp.error) {
+      this.iFramelyData.error = true;
+    }
+    else {
+      this.iFramelyData.error = false;
+      this.iFramelyData.url = iFramelyResp.url;
+      this.iFramelyData.title = iFramelyResp.meta.title;
+      this.iFramelyData.description = iFramelyResp.meta.description;
+      if (iFramelyResp?.links?.thumbnail?.length > 0) {
+        this.iFramelyData.thumbnailLink = iFramelyResp.links.thumbnail[0].href;
+      }
+      if (iFramelyResp?.links?.icon?.length > 0) {
+        this.iFramelyData.iconLink = iFramelyResp.links.icon[0].href;
+      }
+    }
+    return this.iFramelyData;
   }
 }
