79763a706affaf9dbb9ea73d992962d16999f4eb
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-05-25 15:26:19 +0530

    profile icon component added which will show image when it gets loaded

---
+++ src/app/shared/profile-icon/profile-icon.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Renderer2, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-icon',
+  templateUrl: './profile-icon.component.html',
+  styleUrls: ['./profile-icon.component.scss']
+})
+export class ProfileIconComponent implements OnInit {
+  @Input() avatarLink: string;
+  @Input() height: number = 25;
+  loading: boolean = true;
+  @ViewChild('elementOnHTML', { static: false }) elementOnHTML: ElementRef;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.renderer.setStyle(this.elementOnHTML.nativeElement, 'height', this.height + "px");
+  }
+  onLoad() {
+    this.loading = false;
+  }
+}
