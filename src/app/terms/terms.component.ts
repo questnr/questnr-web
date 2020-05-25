6185b0764cfc8f7ba5459af32d3d6e6bf36a5fd2
Satish Kumar Gaur <satishkumargaur@Satishs-MacBook-Air.local>
2020-05-25 23:43:42 +0530

    privacy and terms

--- src/app/terms/terms.component.ts
+++ src/app/terms/terms.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'shared/api.service';
+import {LoginService} from '../auth/login.service';
+import {loggedIn} from '@angular/fire/auth-guard';
 
 @Component({
   selector: 'app-terms',
@@ -8,12 +10,18 @@ import { ApiService } from 'shared/api.service';
   styleUrls: ['./terms.component.scss']
 })
 export class TermsComponent {
-
-  constructor(private router: Router, private api: ApiService) { }
+  loggedIn = false;
+  constructor(private router: Router, private api: ApiService, public loginService: LoginService) { }
 
   goTo(val) {
     this.api.activeAuth = val;
     this.router.navigate(['/']);
   }
-
+  ngOnInit() {
+    if (this.loginService.getUserProfile()){
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
 }
