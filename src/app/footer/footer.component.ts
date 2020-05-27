9b1db51f6da55a961ad791ffffc02fff7ffc329e
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-05-27 19:31:09 +0530

    landing page footer link updated

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalConstants } from 'shared/constants';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  siteTitle: string = GlobalConstants.siteTitle;
+  termsPath: string = GlobalConstants.termsPath;
+  policyPath: string = GlobalConstants.policyPath;
+  supportEmail: string = GlobalConstants.supportEmail;
+  copyRightRenewdYear = GlobalConstants.copyRightRenewdYear;
   constructor() { }
 
   ngOnInit(): void {
