12ea2ef7b6072573aa8012a1a095ac2da4f5e4d6
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-07-26 17:28:20 +0530

    Sign up page footer fixed

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../forgot-password/forgot-password.component.scss']
 })
 export class SignUpPageComponent implements OnInit {
+  mobileView: boolean = false;
+  screenWidth = window.innerWidth;
+  @ViewChild("elementOnHTML") elementOnHTML: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    const width = this.screenWidth;
+    if (width <= 800) {
+      this.mobileView = true;
+    } else if (width >= 1368) {
+      this.mobileView = false;
+    } else if (width >= 800 && width <= 1368) {
+      this.mobileView = false;
+    }
   }
 
+  ngAfterViewInit() {
+    if (this.mobileView) {
+      this.renderer.setStyle(this.elementOnHTML.nativeElement, 'height', '100%');
+      this.renderer.setStyle(this.elementOnHTML.nativeElement, 'margin-top', '20px');
+    } else {
+      this.renderer.setStyle(this.elementOnHTML.nativeElement, 'height', '85%');
+      this.renderer.setStyle(this.elementOnHTML.nativeElement, 'margin-top', '50px');
+    }
+  }
 }
