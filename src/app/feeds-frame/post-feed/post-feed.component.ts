d685ca1880791396b25056feae640e0609d9f10b
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-07-30 22:19:58 +0530

    youtubeId logs removed

--- src/app/feeds-frame/post-feed/post-feed.component.ts
+++ src/app/feeds-frame/post-feed/post-feed.component.ts
@@ -270,7 +270,7 @@ export class PostFeedComponent implements OnInit {
     if (e.target && e.target.value) {
       let detectedLink = this.commonService.parseTextToFindURL(e.target.value);
       let youTubeId = this.commonService.getYouTubeVideoId(detectedLink);
-      console.log("youTubeId", youTubeId);
+      // console.log("youTubeId", youTubeId);
       this.iFramelyService.getIFramelyData(detectedLink).then((iFramelyData: IFramelyData) => {
         this.iFramelyData = iFramelyData;
         // this.metaCardCompRef.setIFramelyData(iFramelyData);
