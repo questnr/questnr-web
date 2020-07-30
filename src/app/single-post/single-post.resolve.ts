ce55dfbe3d18cfd62ac814a41e7fabdcb4e9c827
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-07-30 19:57:49 +0530

    if user has not updated the firstName and lastName, don't suggest null into search bar

--- src/app/single-post/single-post.resolve.ts
+++ src/app/single-post/single-post.resolve.ts
@@ -4,6 +4,7 @@ import { SinglePost } from 'models/single-post.model';
 import { map } from 'rxjs/operators';
 import { UIService } from 'ui/ui.service';
 import { SinglePostService } from './single-post.service';
+import { PostEditorType } from 'models/post-action.model';
 
 @Injectable()
 export class SinglePostResolve implements Resolve<Promise<SinglePost>> {
@@ -12,7 +13,7 @@ export class SinglePostResolve implements Resolve<Promise<SinglePost>> {
 
   resolve(route: ActivatedRouteSnapshot): Promise<SinglePost> {
     return this.singlePostService.getSinglePost(route.paramMap.get('postSlug')).pipe(map((singlePost: SinglePost) => {
-      this.uiService.setMetaTagsAndTitle('Post', singlePost.metaList);
+      this.uiService.setMetaTagsAndTitle(singlePost.postData.postEditorType == PostEditorType.blog ? singlePost.postData.blogTitle : "Post", singlePost.metaList);
       return singlePost;
     })).toPromise();
   }
