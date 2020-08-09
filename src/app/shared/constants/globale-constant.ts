68c966849e7b19dc1965faed9514783bd70c0510
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-08-09 21:49:25 +0530

    modified Questnr description

--- src/app/shared/constants/globale-constant.ts
+++ src/app/shared/constants/globale-constant.ts
@@ -3,7 +3,7 @@ export class GlobalConstants {
   public static siteLink = "https://questnr.com";
   public static siteLogo = "https://questnr.com/assets/logo.png";
   public static siteLogoExternalLink = "https://lh3.googleusercontent.com/pw/ACtC-3cA5INfBc_BXY2vdXrlznXQ1QpPyhvkTx_WeFg21DxvymA0e4x7ZnoGb4UqkvFjnozBF9zbz8SDp5DQhVVFzR6bXB9xwhWLUahDR9hYcfE91XrHgmupHYyaipkmM4Cq0SlQ7r9FweUlGoV6x74ObA7y=s512-no";
-  public static description = "The New Era of Online Communities. Let's discuss what you love or any issues you have, also know similar things from others to improve your skills. Create Post or Blog, or Ask a Question, Now!";
+  public static description = "The New Era of Online Communities. Let's discuss what you love or any issues you have, know similar things from others to improve your skills. Create Post or Blog, or Ask a Question, Now!";
   public static image = '';
   public static fbAppId = "1336590906533811";
   public static gtagId = 'UA-168117128-1';
