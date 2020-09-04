21e830d7c71a6aa8ac5abaa1d7377ec96b633ab8
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-09-04 17:11:26 +0530

    finding the template of FAQ improved

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -14,17 +14,18 @@ export class FAQComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
     private router: Router) {
-    // let faqType = this.route.snapshot.paramMap.get('faqType') as KnowMoreLinkType;
-    // let faqTypeList = Object.keys(KnowMoreLinkType);
+    let faqType = this.route.snapshot.paramMap.get('faqType') as KnowMoreLinkType;
+    let faqTypeList = Object.keys(KnowMoreLinkType);
     // console.log("faqTypeList", faqTypeList, faqType);
-    // for (let index = 0; index < faqTypeList.length; index++) {
-    //   console.log("KnowMoreLinkType[faqTypeList[index]]", KnowMoreLinkType[faqTypeList[index]]);
-    //   if (faqType == KnowMoreLinkType[faqTypeList[index]]) {
-    //     console.log("faqTypeList[index]", index, faqTypeList[index])
-    //     this.faqType = faqTypeList[index] as KnowMoreLinkType;
-    //   }
-    // }
-    this.faqType = this.route.snapshot.paramMap.get('faqType') as KnowMoreLinkType;
+    for (let index = 0; index < faqTypeList.length; index++) {
+      // console.log("KnowMoreLinkType[faqTypeList[index]]", KnowMoreLinkType[faqTypeList[index]]);
+      if (faqType == KnowMoreLinkType[faqTypeList[index]]) {
+        // console.log("faqTypeList[index]", index, faqTypeList[index])
+        this.faqType = KnowMoreLinkType[faqTypeList[index]] as KnowMoreLinkType;
+        break;
+      }
+    }
+    // this.faqType = this.route.snapshot.paramMap.get('faqType') as KnowMoreLinkType;
     // console.log("this.faqType", this.faqType, this.faqType == this.KnowMoreLinkTypeClass.communityPrivacy);
     if (!this.faqType) {
       this.router.navigate(['/', GlobalConstants.error]);
