0ab26e58ac5075b2712bf51a8fcc073547aba87a
Brijeshlakkad <brijeshlakkad22@gmail.com>
2020-10-21 18:14:11 +0530

    ns migrate

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,11 @@
-import { NativeScriptConfig } from '@nativescript/core';
+import { NativeScriptConfig } from '@nativescript/core'
 
 export default {
-	id: 'org.nativescript.ngsample',
-	appResourcesPath: 'App_Resources',
-	android: {
-		v8Flags: '--expose_gc',
-		markingMode: 'none',
-	},
-	appPath: 'src'
-} as NativeScriptConfig;
+  id: 'org.nativescript.ngsample',
+  appResourcesPath: 'App_Resources',
+  android: {
+    v8Flags: '--expose_gc',
+    markingMode: 'none',
+  },
+  appPath: 'src',
+} as NativeScriptConfig
